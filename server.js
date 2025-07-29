3ffdf5c5d48bc24b35d842ec7de3e0f1620c0949
vara prasad <varap2684@gmail.com>
2025-07-29 15:55:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ Act only based on the provided user data. If the user asks something irrelevant 
 `;
 
     // Send to Gemini
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
